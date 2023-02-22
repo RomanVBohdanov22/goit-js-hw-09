19daeda03af8c27197f6678fb09339ee618ebab2
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-22 13:24:35 +0000

    Deploying to gh-pages from @ RomanVBohdanov22/goit-js-hw-09@d4b39471207a3c66ed92e86d6de42e4db67aee63 🚀

---
+++ 02-timer.491b2a16.js
@@ -0,0 +1,2 @@
+document.querySelector("#datetime-picker"),document.querySelector("button[data-start]"),document.querySelector(".timer .value[data-days]"),document.querySelector(".timer .value[data-hours]"),document.querySelector(".timer .value[data-minutes]"),document.querySelector(".timer .value[data-seconds]");
+//# sourceMappingURL=02-timer.491b2a16.js.map
