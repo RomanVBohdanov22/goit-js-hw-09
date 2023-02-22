d4b39471207a3c66ed92e86d6de42e4db67aee63
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-22 15:24:08 +0200

    I don't know where this goes...

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,35 @@
-import _ from 'lodash';
+//import _ from 'lodash';
 
 const inputLnk = document.querySelector('#datetime-picker');
 const buttonStartLnk = document.querySelector('button[data-start]');
 
 const spanDaysLnk = document.querySelector('.timer .value[data-days]');
-console.log("hello");
+const spanHoursLnk = document.querySelector('.timer .value[data-hours]');
+const spanMinutesLnk = document.querySelector('.timer .value[data-minutes]');
+const spanSecondsLnk = document.querySelector('.timer .value[data-seconds]');
+//spanDaysLnk.textContent = 22;
+//console.log("hello");
+
+/*
+    <input type="text" id="datetime-picker" />
+    <button type="button" data-start>Start</button>
+
+    <div class="timer">
+      <div class="field">
+        <span class="value" data-days>00</span>
+        <span class="label">Days</span>
+      </div>
+      <div class="field">
+        <span class="value" data-hours>00</span>
+        <span class="label">Hours</span>
+      </div>
+      <div class="field">
+        <span class="value" data-minutes>00</span>
+        <span class="label">Minutes</span>
+      </div>
+      <div class="field">
+        <span class="value" data-seconds>00</span>
+        <span class="label">Seconds</span>
+      </div>
+    </div>
+*/
