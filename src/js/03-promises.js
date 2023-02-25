0e31a58d19c9314968bedec9517ee4ab4e5d836e
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-25 21:21:16 +0200

    task-03 finalized

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,85 +1,39 @@
-import { result } from 'lodash';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refsToForm = {
-  form: document.querySelector('.form'),
-  delay: document.querySelector('.form input[name="delay"]'),
-  step: document.querySelector('.form input[name="step"]'),
-  amount: document.querySelector('.form input[name="amount"]'),
-  //button: document.querySelector('.form button'),
-};
-
-console.log(refsToForm);
-refsToForm.form.addEventListener('submit', onFormSubmit);
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  let delay = 0;
-  let step = 0;
-  let amount = 0;
+  let delay = +e.target.delay.value;
+  let step = +e.target.step.value;
+  let amount = +e.target.amount.value;
 
-  if (
-    (refsToForm.delay.value >= 0) &&
-    (refsToForm.step.value >= 0) &&
-    (refsToForm.amount.value >= 0)
-  ) {
-    delay = refsToForm.delay.value;
-    step = refsToForm.step.value;
-    amount = refsToForm.amount.value;
-    console.log(`d= ${delay} , s= ${step} , a=${amount}`);
-  } else Notify.failure('Make CORRECT choise, pls! (Must be >=0)');
+  if ([delay, step, amount].find(v => v < 0)) {
+    Notify.failure('Make CORRECT choise, pls! (Must be >=0)');
+    return;
+  }
 
-  //const position = delay * amount + step;
-  let position = 1;
-  for (let i = amount; i > 0; i--) { 
-  
-    const promise = new Promise(createPromise(position, delay), reject);
-      //new Promise((resolve, reject) => );
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
     delay += step;
-    position += 1;
-
-    promise
-      .then(
-        result => { console.log(result); },
-        //error => { console.log(error); },
-      )
-      .catch(error => { console.log(error); },)
-      .finally(() => Notify.warning('I am finally!'));
-      ;
-
+  }
 }
-}
-
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-    if (shouldResolve) {
-    Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`)// Fulfill
-  } else {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)// Reject
-  }
-  }, delay)  
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({position, delay});
+      } else {
+        rej({position, delay});
+      }
+    }, delay);
+  });
 }
-function reject(error) { 
-  console.log(error);
-  Notify.failure(`${error}`);
-}
-/*
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    //console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    //console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });*/
-
-/*
-Notiflix.Notify.success('Sol lucet omnibus');
-
-Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-Notiflix.Notify.warning('Memento te hominem esse');
-
-Notiflix.Notify.info('Cogito ergo sum');
-*/
